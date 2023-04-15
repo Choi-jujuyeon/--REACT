b6b5dc2cf93add83dcf76aa66c18dc2d7c2ccd46
Choi-jujuyeon <chlwndus1216@naver.com>
2023-04-16 01:21:48 +0900

    02-app.js-component-내방식

---
+++ 01-createapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Nav from "./components/nav";
+import Article from "./components/article";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <Article />
+        </div>
+    );
+}
+
+export default App;
---
+++ 01-createapp/src/components/header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <a href="/">REACT</a>
+            </h1>
+        </header>
+    );
+};
+export default Header;
